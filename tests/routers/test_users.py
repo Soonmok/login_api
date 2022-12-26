2dfa233985fc2a5d35ef38e5a21a5db334585fd1
Soonmok <tnsahr2580@gmail.com>
2022-12-26 18:41:51 +0900

    로그인 절차 테스트 코드 추가

--- tests/routers/test_users.py
+++ tests/routers/test_users.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 
 
-class TestUserSignin:
+class TestUserRouter:
     phone_number = "01011111111"
     auth_code = ""
 
@@ -57,3 +57,23 @@ class TestUserSignin:
         response = app_client.post("/users/signup", json=payload)
         assert response.status_code == 400
         assert response.json()['detail'] == 'SMS code is not valid'
+
+    def test_login(self, app_client: TestClient) -> None:
+        # email을 통한 로그인을 구현하였으므로, username에는 email을 넣어야 한다.
+        payload = {
+            "username": "tnsahr2580",
+            "password": "password1",
+        }
+        response = app_client.post("/token", data=payload)
+        assert response.status_code == 200
+        assert response.json()['access_token']
+
+    def test_login_with_invalid_password(self, app_client: TestClient) -> None:
+        # email을 통한 로그인을 구현하였으므로, username에는 email을 넣어야 한다.
+        payload = {
+            "username": "tnsahr2580",
+            "password": "password12",
+        }
+        response = app_client.post("/token", data=payload)
+        assert response.status_code == 401
+        assert response.json()['detail'] == 'Incorrect username or password'
