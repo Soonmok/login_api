881b701489c34dbac30794da340596552cc1de17
Soonmok <tnsahr2580@gmail.com>
2022-12-26 11:34:50 +0900

    sms 전송 이력을 저장하는 테이블 추가

---
+++ app/db/models/sms.py
@@ -0,0 +1,17 @@
+import uuid
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+from typing import Any
+from sqlalchemy.ext.declarative import declarative_base
+
+Base: Any = declarative_base()
+
+
+class SMS(Base):
+    __tablename__ = "sms"
+
+    sms_id = sa.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    phone = sa.Column(sa.Text, nullable=False)
+    auth_code = sa.Column(sa.Text, nullable=False)
+    created_at = sa.Column(sa.DateTime, server_default=sa.func.now(), nullable=False)
